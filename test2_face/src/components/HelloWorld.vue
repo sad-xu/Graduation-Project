76677c4ba863aa90a7bade06c59e232fc012ef84
xhc <1031568754@qq.com>
2018-03-07 01:25:18 +0800

    add test2_face socket && camera

--- test2_face/src/components/HelloWorld.vue
+++ test2_face/src/components/HelloWorld.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="main">
-    <video id="video" width="300" height="300"></video>
-    <canvas id="canvas" width="300" height="300"></canvas>
+    <input type="button" @click="sendImg('img1')" value="send"/>
+    <video id="video" width="300" height="300" autoplay></video>
+    <canvas id="canvas" width="300" height="200"></canvas>
+    <input type="button" value="startVideo" @click="startVideo()">
+    <input type="button" value="getimg" @click="getImg()">
+    <input type="button" value="stop" @click="stop()">
   </div>
 </template>
 
@@ -12,7 +16,58 @@ export default {
     return {
       info: `1.检测摄像头，若无摄像头则切换成图片上传模式
              2.开启摄像，定时上传截图（socket），服务器返回坐标
-             3.接收坐标，显示`
+             3.接收坐标，显示`,
+      video: null,
+      canvas: null,
+      context: null
+    }
+  },
+  mounted() {
+    this.video = document.getElementById('video');
+    this.canvas = document.getElementById('canvas');
+    this.context = this.canvas.getContext('2d'); 
+  },
+  sockets: {
+    connect: () => {
+      console.log('connected success')
+    },
+    sSend: (val) => {
+      console.log(val)
+    }
+  },
+  methods: {
+    sendImg: function(val) {  // 发送图片
+      this.$socket.emit('cSend', val)
+    },
+    getImg: function() {    // 获取图片
+      this.context.drawImage(this.video, 0, 0, 300, 200);
+    },
+    startVideo: function() {  // 开启摄像头
+      //获得摄像头并显示到video区域
+      let video = document.getElementById('video');
+      let videoObj = {"video": true};
+      let errBack = function() {
+        console.log('video failed')
+      };
+      if (navigator.webkitGetUserMedia) { // WebKit-prefixed  
+          navigator.webkitGetUserMedia(videoObj, function(stream) {  
+              video.src = window.URL.createObjectURL(stream);  
+              video.play();  
+          }, errBack);  
+      } else if (navigator.mozGetUserMedia) { // Firefox-prefixed  
+          navigator.mozGetUserMedia(videoObj, function(stream) {  
+              video.src = window.URL.createObjectURL(stream);  
+              video.play();  
+          }, errBack);  
+      } else if (navigator.getUserMedia) { // Standard  
+          navigator.getUserMedia(videoObj, function(stream) {  
+            video.src = stream;
+            video.play();     
+          }, errBack);  
+      }  
+    },
+    stop: function() {
+      this.video.pause();
     }
   }
 }
--- test2_face/src/main.js
+++ test2_face/src/main.js
@@ -3,8 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueSocketio from 'vue-socket.io'
 
 Vue.config.productionTip = false
+Vue.use(VueSocketio, 'http://127.0.0.1:8082')
 
 /* eslint-disable no-new */
 new Vue({
