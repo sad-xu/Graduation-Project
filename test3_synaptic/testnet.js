58c9fc2e4d57a9e2f9de7500afcea5437a8cbde7
xhc <1031568754@qq.com>
2018-06-11 21:20:19 +0800

    finish!

--- test3_synaptic/testnet.js
+++ test3_synaptic/testnet.js
@@ -1,33 +1,33 @@
 const fs = require('fs')
 const cv = require('opencv4nodejs')
 const emnist = require('./emnist')
-const set = emnist.set(2, 1456)    // 2080 训练  1680 测试
+const set = emnist.set(2, 1)    // 2080 训练  1680 测试
 
 const trainingSet = set.training
 const testSet = set.test
 
 console.log(testSet.length)
-// testSet.forEach(function(item, index) {
-// 	let pic = item.input,
-// 			picArr = [[]];
-// 	pic.forEach(function(ite, ind) {
-// 		ite = Math.abs(ite * 255 - 255);
-// 		if (picArr[picArr.length-1].length < 28) {
-// 			picArr[picArr.length-1].push(ite)
-// 		} else {
-// 			picArr.push([ite])
-// 		}
-// 	})
-// 	let picMat = new cv.Mat(picArr, cv.CV_8UC1);
-// 	let rotate = cv.getRotationMatrix2D(new cv.Point(14,14), -90)
-// 	picMat= picMat.warpAffine(rotate)
-// 	picMat = picMat.flip(1);  // 镜像旋转   0 x  1 y   -1 x+y
-// 	cv.imshow('a'+index, picMat)
-// })
-// cv.waitKey();
+testSet.forEach(function(item, index) {
+	let pic = item.input,
+			picArr = [[]];
+	pic.forEach(function(ite, ind) {
+		ite = Math.abs(ite * 255 - 255);
+		if (picArr[picArr.length-1].length < 28) {
+			picArr[picArr.length-1].push(ite)
+		} else {
+			picArr.push([ite])
+		}
+	})
+	let picMat = new cv.Mat(picArr, cv.CV_8UC1);
+	// let rotate = cv.getRotationMatrix2D(new cv.Point(14,14), -90)
+	// picMat= picMat.warpAffine(rotate)
+	// picMat = picMat.flip(1);  // 镜像旋转   0 x  1 y   -1 x+y
+	cv.imshow('a'+index, picMat)
+})
+cv.waitKey();
 
 
-
+/*
 let data = fs.readFileSync('./nets/net.js', 'utf8')
 const net = eval('(' + data + ')');
 
@@ -64,7 +64,7 @@ console.log('总数：', num, '通过数：', suc)
 console.log('成功率：', suc/num)
 console.log('耗时', new Date() - now)
 
-
+*/
 
 
 
